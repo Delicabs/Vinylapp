5055c7b5076b2b6bea5eaad4468b5393e094ebcc
Juan Cabria <Juan.Cabria@gmail.com>
2019-04-08 19:09:47 +0300

    first commit

--- src/main/java/com/example/vinska/domain/User.java
+++ src/main/java/com/example/vinska/domain/User.java
@@ -3,6 +3,7 @@ package com.example.vinska.domain;
 import javax.persistence.*;
 
 @Entity
+@Table(name="Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
